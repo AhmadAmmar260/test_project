ef8af8e084c4bb5796b3cce424b9a0a98aaab781
Ahmed Ammar <AhmadAljawahery@gmail.com>
2021-09-21 23:53:15 +0300

    add User Registration

--- test_project/urls.py
+++ test_project/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
    path('type_of_account',index,name='index'),
    path('login',login,name='login'),
+   path('register',register,name='register'),
    path('test',adding)
 ]
--- test_project/views.py
+++ test_project/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse ,JsonResponse
 from django.contrib.auth.models import User
+
 def index(request):
     return render(request,'main.html')
 
 
 def login(request):
+    
     return render(request,"login.html")
+
+
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
@@ -20,3 +24,19 @@ def adding(request):
     }
     
     return JsonResponse(data)
+def register(request):
+    if request.method == "POST":
+        username  = request.POST.get('username')
+        email = request.POST['email']
+        password  = request.POST['password']
+        print(username,email,password)
+        user = User.objects.create_user(username,email,password)
+        user.last_name = 'me'
+        user.save()
+        return redirect('/login')
+    else:
+        return render(request,'Register.html')
+       
+    
+       
+    
